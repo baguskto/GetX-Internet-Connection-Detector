cd5f62f30bb3656ee2c829ecfa2af55a53451c0c
baguskto <baguskto@gmail.com>
2021-06-11 15:41:12 +0700

    feat: unused App routes

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,15 @@
+
+import 'package:get/get.dart';
+part './app_routes.dart';
+
+
+// class AppPages {
+//   List<GetPage> getPages(String routeName) {
+//     return [
+//       GetPage(
+//           name: AppRoutes.ROOT,
+//           binding: RootBinding(),
+//           page: () => Root(routeName: routeName)),
+//     ];
+//   }
+// }
